353d6017a13d0538db4c695fbca6d346e2c006e5
shockstate <guillem.bonafonte@gmail.com>
2015-11-12 20:27:31 +0000

    Add users and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  root 'application#hello'
+  root 'users#index'
   # Example resource route with options:
   #   resources :products do
   #     member do
